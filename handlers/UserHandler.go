c8a5a1a8566e00e51e9a6e276328f3532e734f9f
fklalita <FkLalita>
2024-04-24 04:43:49 +0100

    added handlers

---
+++ handlers/UserHandler.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/FkLalita/hano/models"
+	"github.com/FkLalita/hano/utils"
+
+	"github.com/FkLalita/hano/templates"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CreateUserHandler(db *sql.DB, e echo.Context) error {
+	username := e.FormValue("username")
+	models.CreateUser(db, username)
+
+	return utils.Render(e, http.StatusOK, templates.Home())
+
+}
