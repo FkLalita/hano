bfa76415080099b7fb246dd793e30ddf72323113
fklalita <FkLalita>
2024-04-24 04:42:34 +0100

    nothing

---
+++ models/User.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+  "database/sql"
+  "log"
+)
+
+type User struct {
+  username string
+}
+
+func CreateUser(db *sql.DB, username string)  {
+  _, err := db.Exec("INSERT INTO User (user_name) VALUES (?, ?, ?)", username)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
