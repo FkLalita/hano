0d39516a9e3982ff22ed8d90a503ece5848a1645
fklalita <FkLalita>
2024-04-24 04:49:21 +0100

    added utils

--- utils/db.go
+++ utils/db.go
@@ -1,14 +1,14 @@
 package utils
 
 import (
-  "database/sql"
-  "fmt"
+	"database/sql"
+	"fmt"
 )
 
 func CreateTable(db *sql.DB) error {
-  // Execute SQL statement to create table
+	// Execute SQL statement to create table
 
-  _, err := db.Exec(`
+	_, err := db.Exec(`
         CREATE TABLE IF NOT EXISTS Topics  (
             post_id INT PRIMARY KEY AUTO_INCREMENT,
             topic_title VARCHAR(255),
@@ -17,28 +17,24 @@ func CreateTable(db *sql.DB) error {
         );
     `)
 
+	if err != nil {
+		fmt.Println("Error creating  topic table:", err)
+		return err
+	}
 
-    if err != nil {
-        fmt.Println("Error creating  topic table:", err)
-        return err
-    }
-
-
-    _, err = db.Exec(`
+	_, err = db.Exec(`
        CREATE TABLE IF NOT EXISTS User (
             user_id INT PRIMARY KEY AUTO_INCREMENT,
             user_name VARCHAR(50)
       );    
   `)
 
-    if err != nil {
-        fmt.Println("Error creating user table:", err)
-        return err
-    } 
+	if err != nil {
+		fmt.Println("Error creating user table:", err)
+		return err
+	}
 
-
-
-    _, err = db.Exec(`
+	_, err = db.Exec(`
        CREATE TABLE IF NOT EXISTS ChatMessage (
             message_id INT PRIMARY KEY AUTO_INCREMENT,
             post_id INT,
@@ -49,12 +45,12 @@ func CreateTable(db *sql.DB) error {
             FOREIGN KEY (user_id) REFERENCES User(user_id)
       );
   `)
-    if err != nil {
-        fmt.Println("Error creating  chat table:", err)
-        return err
-    }
-    fmt.Println("table created successfully")
+	if err != nil {
+		fmt.Println("Error creating  chat table:", err)
+		return err
+	}
+	fmt.Println("table created successfully")
 
-    return nil    
-    
+	return nil
+
 }
