d7e4b9b9764e382f07f28cf01373638e9d7aa23c
fklalita <FkLalita>
2024-04-25 05:36:40 +0100

    empt

--- handlers/StoryHandler.go
+++ handlers/StoryHandler.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/FkLalita/hano/models"
+	"github.com/FkLalita/hano/templates"
+	"github.com/FkLalita/hano/utils"
+	"github.com/labstack/echo/v4"
+)
+
+func GetTopicsHandler(db *sql.DB, e echo.Context) error {
+
+	// Retrieve all topics from the database
+	topics, err := models.GetTopics(db)
+	if err != nil {
+		// If there's an error retrieving topics, return an internal server error response
+		return e.String(http.StatusInternalServerError, "Failed to retrieve topics from the database")
+	}
+
+	return utils.Render(e, http.StatusOK, templates.Home(topics))
+
+}
