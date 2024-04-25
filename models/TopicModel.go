895dad460accedc585d6076f9360d398f44f3ff7
fklalita <FkLalita>
2024-04-25 01:49:30 +0100

    added storymodel

--- models/TopicModel.go
+++ models/TopicModel.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"database/sql"
+	"time"
+
+	"log"
+)
+
+type Topic struct {
+	Title       string
+	Description string
+	CreatedAt   time.Time
+}
+
+func CreateTopic(db *sql.DB, title string, description string) {
+	_, err := db.Exec("INSERT INTO Topics (title, description) VALUES (?,?) ", title, description)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
